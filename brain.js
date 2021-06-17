c55d910f7fd9eab5eeb3704244454626408a9aa7
Noob Science <78465651+newtoallofthis123@users.noreply.github.com>
2021-06-17 11:33:34 +0530

    Awesome New UI Update

---
+++ brain.js
@@ -0,0 +1,7 @@
+const x = document.getElementById('button')
+const theme = document.querySelector('.light-dark')
+const y = document.getElementById('light-dark')
+
+x.addEventListener('click', function name(params) {
+	document.body.classList.toggle('black');
+})
